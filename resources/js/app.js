3a47231c3fd7888d701ef2bf64e97f0ccd652f35
Pradibta4 <gansprayo@gmail.com>
2026-03-05 09:15:11 +0700

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App.vue';
+
+const appElement = document.getElementById('app');
+
+if (appElement) {
+    createApp(App).mount(appElement);
+}
