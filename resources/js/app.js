21e61dc18f7c7f1c0a7e6929ceff074ed1b7496a
ufftz4s <josephinbaru7@gmail.com>
2026-04-27 07:56:15 +0700

    cleanup: remove old laravel-vue resources

--- resources/js/app.js
+++
@@ -1,9 +0,0 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import App from './components/App.vue';
-
-const appElement = document.getElementById('app');
-
-if (appElement) {
-    createApp(App).mount(appElement);
-}
